36207f5a8a020924cc07c5a6bc42dafe8a680515
unknown <christianvatvt@gmail.com>
2024-11-12 14:34:10 -0300

    test

--- src/test/java/com/christianvladimir/todosimple/TodosimpleApplicationTests.java
+++ src/test/java/com/christianvladimir/todosimple/TodosimpleApplicationTests.java
@@ -1,4 +1,4 @@
-package com.christianvladimir.simpletodo;
+package com.christianvladimir.todosimple;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
