e517b423c788a095fc8abb8496b3348270148c6f
unknown <christianvatvt@gmail.com>
2024-11-12 15:31:46 -0300

    User Repository

---
+++ src/main/java/com/christianvladimir/todosimple/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.christianvladimir.todosimple.repositories;
+
+import com.christianvladimir.todosimple.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
